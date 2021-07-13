438c5d2a5604d9f81ee80b2832ecf6639f344c34
Tarcisio Delmondes <tarcisiodelmondes@gmail.com>
2021-07-12 23:37:02 -0300

    Aula create server

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
-console.log("Hello world");
+import { createServer } from "http";
+
+const server = createServer((req, res) => {
+  switch (req.url) {
+    case "/":
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(
+        JSON.stringify({
+          name: "Tarcisio",
+          lastName: "Delmondes",
+        })
+      );
+      res.end();
+      break;
+
+    default:
+      res.writeHead(404);
+      res.write("Not found");
+      res.end();
+      break;
+  }
+});
+
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+const HOSTNAME = process.env.HOSTNAME || "127.0.0.1";
+
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server is listening at http://${HOSTNAME}:${PORT}/`);
+});
