4c2d1ec8a75c52de5f664d74915fd4f21faf8c1c
Tarcisio Delmondes <tarcisiodelmondes@gmail.com>
2021-07-13 14:39:57 -0300

    Aula rendering, parameters and redirect

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
+import { readFile } from "fs";
 import { createServer } from "http";
+import path from "path";
+import { parse } from "querystring";
 
 const server = createServer((req, res) => {
   switch (req.url) {
@@ -13,6 +16,35 @@ const server = createServer((req, res) => {
       res.end();
       break;
 
+    case "/sign-in":
+      getStaticFileContent(res, "pages/sign-in.html", "text/html");
+
+      break;
+
+    case "/authenticate":
+      let data = "";
+      req.on("data", (chunk) => {
+        data += chunk;
+      });
+      req.on("end", () => {
+        res.writeHead(301, { location: "/home" });
+        res.end();
+      });
+
+      break;
+
+    case "/home":
+      getStaticFileContent(res, "pages/home.html", "text/html");
+      break;
+
+    case "/global.css":
+      getStaticFileContent(res, "styles/global.css", "text/css");
+      break;
+
+    case "/sign-in.css":
+      getStaticFileContent(res, "styles/pages/sign-in.css", "text/css");
+      break;
+
     default:
       res.writeHead(404);
       res.write("Not found");
@@ -20,6 +52,22 @@ const server = createServer((req, res) => {
       break;
   }
 });
+
+function getStaticFileContent(res, filePath, contentType) {
+  filePath = path.resolve(__dirname, filePath);
+
+  readFile(filePath, (err, file) => {
+    if (err) {
+      res.writeHead(500, "Internal server error");
+      res.end();
+      return;
+    }
+
+    res.writeHead(200, { "Content-Type": contentType });
+
+    res.end(file);
+  });
+}
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 const HOSTNAME = process.env.HOSTNAME || "127.0.0.1";
